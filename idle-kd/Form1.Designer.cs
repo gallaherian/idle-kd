eb34df8ff8beca1eee2693ef81b437cf5ac52181
gallaherian <gallaher.ian@gmail.com>
2021-08-13 10:46:31 +0100

    Idle KD Ratio Initial Commit
    Initial Commit for Idle KD applet

---
+++ idle-kd/Form1.Designer.cs
@@ -0,0 +1,135 @@
+﻿namespace idleKD
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.focusLabel = new System.Windows.Forms.Label();
+            this.idleLabel = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.totLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label1.Location = new System.Drawing.Point(0, 0);
+            this.label1.Margin = new System.Windows.Forms.Padding(0);
+            this.label1.Name = "label1";
+            this.label1.Padding = new System.Windows.Forms.Padding(18, 2, 0, 0);
+            this.label1.Size = new System.Drawing.Size(122, 27);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "00:00:00";
+            // 
+            // focusLabel
+            // 
+            this.focusLabel.AutoSize = true;
+            this.focusLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.focusLabel.ForeColor = System.Drawing.Color.Green;
+            this.focusLabel.Location = new System.Drawing.Point(0, 25);
+            this.focusLabel.Margin = new System.Windows.Forms.Padding(0);
+            this.focusLabel.Name = "focusLabel";
+            this.focusLabel.Size = new System.Drawing.Size(57, 13);
+            this.focusLabel.TabIndex = 1;
+            this.focusLabel.Text = "00:00:00";
+            // 
+            // idleLabel
+            // 
+            this.idleLabel.AutoSize = true;
+            this.idleLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.idleLabel.ForeColor = System.Drawing.Color.Red;
+            this.idleLabel.Location = new System.Drawing.Point(76, 25);
+            this.idleLabel.Margin = new System.Windows.Forms.Padding(0);
+            this.idleLabel.Name = "idleLabel";
+            this.idleLabel.Size = new System.Drawing.Size(57, 13);
+            this.idleLabel.TabIndex = 2;
+            this.idleLabel.Text = "00:00:00";
+            this.idleLabel.MouseHover += new System.EventHandler(this.idleLabel_MouseHover);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(10, 54);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(110, 23);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Reset";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // totLabel
+            // 
+            this.totLabel.AutoSize = true;
+            this.totLabel.Location = new System.Drawing.Point(2, 38);
+            this.totLabel.Name = "totLabel";
+            this.totLabel.Size = new System.Drawing.Size(35, 13);
+            this.totLabel.TabIndex = 4;
+            this.totLabel.Text = "label2";
+            this.totLabel.Click += new System.EventHandler(this.totLabel_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.ClientSize = new System.Drawing.Size(132, 80);
+            this.ControlBox = false;
+            this.Controls.Add(this.totLabel);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.idleLabel);
+            this.Controls.Add(this.focusLabel);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Location = new System.Drawing.Point(1230, 689);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.Opacity = 0.7D;
+            this.ShowIcon = false;
+            this.ShowInTaskbar = false;
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.Manual;
+            this.Text = "Form1";
+            this.TopMost = true;
+            this.MouseEnter += new System.EventHandler(this.Form1_MouseEnter);
+            this.MouseLeave += new System.EventHandler(this.Form1_MouseLeave);
+            this.MouseHover += new System.EventHandler(this.Form1_MouseHover);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label focusLabel;
+        private System.Windows.Forms.Label idleLabel;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label totLabel;
+    }
+}
+
---
+++ idle-kd/Form1.cs
@@ -0,0 +1,198 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Collections;
+using System.Runtime.InteropServices;
+using System.Configuration;
+using System.Collections.Specialized;
+
+namespace idleKD
+{
+    public partial class Form1 : Form
+    {
+        Timer timer;
+        DateTime lastIdle;
+        double largestIdle_s = 0;
+        double largestFocus_s = 0;
+        int idleThreshold_s = 60;
+        DateTime lunchStart, lunchEnd;
+        int updatePeriod_ms = 500;
+        int lunchIdle = 0;
+        int totalIdleTime_ms = 0;
+        int totalTime_ms;
+        DateTime lastReset = DateTime.Now;
+
+
+        static int totalIdleTime = 0;
+        [DllImport("user32.dll")]
+        static extern bool GetLastInputInfo(out LASTINPUTINFO plii);
+
+        [DllImport("User32.Dll")]
+        public static extern long SetCursorPos(int x, int y);
+
+        [StructLayout(LayoutKind.Sequential)]
+        public struct POINT
+        {
+            public int X;
+            public int Y;
+
+            public static implicit operator Point(POINT point)
+            {
+                return new Point(point.X, point.Y);
+            }
+        }
+
+        [DllImport("user32.dll")]
+        public static extern bool GetCursorPos(out POINT lpPoint);
+
+        [StructLayout(LayoutKind.Sequential)]
+        struct LASTINPUTINFO
+        {
+            public static readonly int SizeOf =
+                   Marshal.SizeOf(typeof(LASTINPUTINFO));
+
+            [MarshalAs(UnmanagedType.U4)]
+            public int cbSize;
+            [MarshalAs(UnmanagedType.U4)]
+            public int dwTime;
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            idleThreshold_s = Convert.ToInt32(Properties.Settings.Default["IdleThreshold_s"]);
+            updatePeriod_ms = Convert.ToInt32(Properties.Settings.Default["UpdatePeriod_ms"]);
+            lunchStart = Convert.ToDateTime(Properties.Settings.Default["LunchTimeStart"]);
+            lunchEnd = Convert.ToDateTime(Properties.Settings.Default["LunchTimeEnd"]);
+
+            if (Convert.ToInt32(Properties.Settings.Default["LastDay"]) == DateTime.Now.Day)
+            {
+                largestFocus_s = Convert.ToInt32(Properties.Settings.Default["LastFocus"]);
+                largestIdle_s = Convert.ToInt32(Properties.Settings.Default["LastIdle"]);
+            }
+
+            label1.Text = "";
+            timer = new Timer();
+            timer.Interval = updatePeriod_ms;
+            timer.Tick += Timer_Tick;
+            timer.Start();
+
+            lastIdle = DateTime.Now;
+        }
+
+        private void idleLabel_MouseHover(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_MouseHover(object sender, EventArgs e)
+        {
+            //this.Visible = false;
+        }
+
+        private void Form1_MouseLeave(object sender, EventArgs e)
+        {
+            //this.Visible = true;
+        }
+
+        private void Form1_MouseEnter(object sender, EventArgs e)
+        {
+            //this.Visible = false;
+            //var currentTime = DateTime.Now;
+            //while((DateTime.Now - currentTime).TotalSeconds < 5)
+            //{
+            //}
+            //this.Visible = true;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            totalTime_ms = 0;
+            largestIdle_s = 0;
+            largestFocus_s = 0;
+        }
+
+        private void totLabel_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            int idleTime = 0;
+            LASTINPUTINFO lastInputInfo = new LASTINPUTINFO();
+            lastInputInfo.cbSize = Marshal.SizeOf(lastInputInfo);
+            lastInputInfo.dwTime = 0;
+            //totalTime_ms += updatePeriod_ms;
+
+            int envTicks = Environment.TickCount;
+
+            if (GetLastInputInfo(out lastInputInfo))
+            {
+                int lastInputTick = lastInputInfo.dwTime;
+                idleTime = envTicks - lastInputTick;
+                //totalIdleTime_ms += idleTime;
+            }
+
+            // Calc total ToT
+            totLabel.Text = "TOT: " + TimeSpan.FromMilliseconds(totalTime_ms - totalIdleTime_ms).ToString(@"hh\:mm\:ss");
+
+            if (idleTime > (idleThreshold_s * 1000)) // Been idle for longer than threshold
+            {
+                if (DateTime.Compare(DateTime.Now, lunchStart) == 1 && DateTime.Compare(DateTime.Now, lunchEnd) == -1)
+                {
+                    this.label1.Text = "  BREAK";
+                    this.label1.ForeColor = Color.DarkGray;
+                    lastIdle = DateTime.Now;
+                    return;
+                }
+                else if (DateTime.Compare(DateTime.Now, lunchEnd) == 1 && (DateTime.Now - lunchEnd).TotalMilliseconds < updatePeriod_ms)
+                {
+                    // If we have JUST exited lunch, and are still in a idle period
+                    lunchIdle = idleTime;
+                }
+
+                var time = TimeSpan.FromMilliseconds(idleTime - lunchIdle);
+                this.label1.Text = time.ToString(@"hh\:mm\:ss");
+                this.label1.ForeColor = Color.Red;
+                lastIdle = DateTime.Now;
+
+                if (time.TotalSeconds > largestIdle_s)
+                    largestIdle_s = (int)time.TotalSeconds;
+            }
+            else
+            {
+                totalTime_ms += updatePeriod_ms;
+                lunchIdle = 0;
+                var duration = (DateTime.Now - lastIdle);
+
+                if (duration.TotalSeconds > largestFocus_s)
+                    largestFocus_s = duration.TotalSeconds;
+                
+                this.label1.Text = duration.ToString(@"hh\:mm\:ss");
+                this.label1.ForeColor = Color.Green;
+            }
+
+            var idleSpan = TimeSpan.FromSeconds(largestIdle_s);
+            var focusSpan = TimeSpan.FromSeconds(largestFocus_s);
+            focusLabel.Text = focusSpan.ToString(@"hh\:mm\:ss");
+            idleLabel.Text = idleSpan.ToString(@"hh\:mm\:ss");
+
+            this.BringToFront(); // Keep window in front of others
+
+            //percLabel.Text = ((int)(100 * (double)totalTime_ms / (double)totalIdleTime_ms)).ToString() + "%";
+
+            Properties.Settings.Default["LastFocus"] = ((int)largestFocus_s).ToString();
+            Properties.Settings.Default["LastIdle"] = ((int)largestIdle_s).ToString();
+            Properties.Settings.Default["LastDay"] = DateTime.Now.Day.ToString();
+            Properties.Settings.Default.Save(); // Saves settings in application configuration file
+        }
+    }
+}
